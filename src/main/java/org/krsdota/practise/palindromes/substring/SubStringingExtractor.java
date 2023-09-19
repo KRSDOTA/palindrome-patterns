11194dfe2e7bb5a8713e49b28dc386a412b56ae4
Rupert Hammond <rhammond@caci.co.uk>
2023-09-19 07:22:33 +0100

    added missing Javadoc

--- src/main/java/org/krsdota/practise/palindromes/substring/SubStringingExtractor.java
+++ src/main/java/org/krsdota/practise/palindromes/substring/SubStringingExtractor.java
@@ -4,6 +4,12 @@ import java.util.List;
 
 public interface SubStringingExtractor {
 
+    /**
+     * Given some input string, extract all contiguous sub-stings
+     *
+     * @param inputString input string
+     * @return constituent sub-strings
+     */
     public List<String> extractContiguousSubStrings(String inputString);
 
 }
